b84fc8ad0565cb67f279ca061d1e98f953315c52
JJackson <gosttmgo45@gmail.com>
2026-01-13 20:30:48 +0900

    feat : 델리게이트 문법 기초

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LiveCode
+{
+    // 델리게이트
+    // - 메서드 참조를 지정할 수 있는 형식
+
+    public class Program
+    {
+        //선언
+        private delegate void LoggerDelegate(string msg);
+
+        static void Main(string[] args)
+        {
+            // 2. 델리게이트 할당
+            //LoggerDelegate log = Logger;
+            LoggerDelegate? log = null;
+
+            //3. 델리게이트 호출
+            //log("델리게이트 호출 성공 1");
+
+            //4. 델리게이트 체인 기법
+            log += Logger;
+            log += LoggerTime;
+            
+            log?.Invoke("델리게이트 호출 성공 : 체인 ");
+
+            Console.WriteLine("정상 종료");
+
+            // 5. 델리게이트 제거
+            log -= Logger;
+            log?.Invoke("델리게이트 호출 성공 : 체인 제거 후 ");
+        }
+
+        static void Logger(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        static void LoggerTime(string msg)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {msg}");
+        }
+    }
+}
